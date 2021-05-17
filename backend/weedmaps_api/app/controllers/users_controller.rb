a2b1b275742a0e319201e97552c4a656a00c852f
Eva <yi.zheng1709@gmail.com>
2021-05-17 01:57:17 -0400

    nests id_Cards and medical_recommendations in users routes in backend; sets up users controller to accept params

--- backend/weedmaps_api/app/controllers/users_controller.rb
+++ backend/weedmaps_api/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
-    
+    def index 
+        if params["search"]
+            # users = User.where("first_name")
+        end
+    end
 
 end
--- backend/weedmaps_api/config/routes.rb
+++ backend/weedmaps_api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :id_cards
-  resources :medical_recommendations
-  resources :users
+  resources :users, do 
+    resources :id_cards
+    resources :medical_recommendations
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
